1cd4dde7083ad78f95b38fdc9cd785a4045e4b23
mirkobechini <mirkobechini@gmail.com>
2025-08-04 17:51:15 +0200

    handle api with for

--- assets/main.js
+++ assets/main.js
@@ -7,11 +7,23 @@ generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
 
 //url api
+const api_url_mail = 'https://flynn.boolean.careers/exercises/api/random/mail'
+
 
 //dom element
 
+const mailListEl = document.getElementById("mailList")
 
-//api request
 
-
-//dom population
+for (let i = 0; i < 10; i++) {
+    //api request
+    axios.get(api_url_mail)
+        .then(response => {
+            const mail = response.data.response
+            
+            //dom population
+            mailListEl.insertAdjacentHTML('beforeend', `
+            <li> ${mail} <li>
+            `)
+        })
+}
