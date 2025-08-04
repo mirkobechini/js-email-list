22ff7616b729cc542ed3d08736c86f8ce5223080
mirkobechini <mirkobechini@gmail.com>
2025-08-04 18:11:58 +0200

    add button for new mail list

--- assets/main.js
+++ assets/main.js
@@ -13,17 +13,33 @@ const api_url_mail = 'https://flynn.boolean.careers/exercises/api/random/mail'
 //dom element
 
 const mailListEl = document.getElementById("mailList")
+const refreshMailEl = document.getElementById("refreshMail")
+
+generateMail()
 
 
-for (let i = 0; i < 10; i++) {
-    //api request
-    axios.get(api_url_mail)
-        .then(response => {
-            const mail = response.data.response
-            
-            //dom population
-            mailListEl.insertAdjacentHTML('beforeend', `
-            <li> ${mail} <li>
+refreshMailEl.addEventListener('click', ()=>{
+    return generateMail()
+})
+
+
+
+
+
+
+
+//functions
+function generateMail() {
+    for (let i = 0; i < 10; i++) {
+        //api request
+        axios.get(api_url_mail)
+            .then(response => {
+                const mail = response.data.response
+
+                //dom population
+                mailListEl.insertAdjacentHTML('beforeend', `
+            <li class="list-group-item"> ${mail} </li>
             `)
-        })
+            })
+    }
 }
